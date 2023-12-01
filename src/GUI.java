a140d383f788427379d0d9eeed7db439cd84e803
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-12-01 19:08:54 +0100

    la till knapp till frame + satte mer egenskaper på frame

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
     private JFrame frame = new JFrame();
     private JButton chooseBTN = new JButton();
     private JTable table = new JTable();
     public GUI(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500,1500);
+        frame.setLayout(new BorderLayout());
+        frame.add(chooseBTN,BorderLayout.NORTH);
 
+
+        frame.setVisible(true);
     }
 }
