b29881878194a819a65263bd5ccee66ffe1c67b7
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-12-01 19:05:05 +0100

    la till GUI med table, frame, button

---
+++ src/GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUI {
+    private JFrame frame = new JFrame();
+    private JButton chooseBTN = new JButton();
+    private JTable table = new JTable();
+    public GUI(){
+
+    }
+}
